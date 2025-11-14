b969d83f92dcae2ba337839e236690e8f55c007b
raggy <raggynexli@gmail.com>
2025-11-14 21:58:40 +0530

    update dependencies and fix bugs in index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import express from "express";
 import cors from "cors";
 import { google } from "googleapis";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
